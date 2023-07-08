0a76442b53172359665034310263dd5b2a75ef4d
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-08 14:24:23 +0330

    Add Category Icons To expense With Fixed Error

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -6,11 +6,11 @@ const uuid = Uuid();
 enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
-  Category.food : Icons.lunch_dining,
-  Category.travel : Icons.flight_takeoff,
-  Category.leisure : Icons.movie,
-  Category.work : Icons.work,
-}
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   Expense({
