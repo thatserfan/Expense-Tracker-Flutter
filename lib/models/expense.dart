dbecf61cfcbeda98b3a9a5b6008c83ce03cc319e
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-08 14:23:21 +0330

    Add Category Icons To expense

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 const uuid = Uuid();
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food : Icons.lunch_dining,
+  Category.travel : Icons.flight_takeoff,
+  Category.leisure : Icons.movie,
+  Category.work : Icons.work,
+}
 
 class Expense {
   Expense({
