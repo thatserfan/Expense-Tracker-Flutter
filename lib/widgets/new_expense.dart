708db015fc1a3ab1079d5456c6e891870da9582c
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-09 15:51:10 +0330

    Add Dropdown With Logic

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -14,6 +14,7 @@ class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
   DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
 
   void _presentDatePicker() async {
     final now = DateTime.now();
@@ -81,6 +82,27 @@ class _NewExpenseState extends State<NewExpense> {
         ),
         Row(
           children: [
+            const SizedBox(height: 16),
+            DropdownButton(
+              value: _selectedCategory,
+              items: Category.values
+                  .map(
+                    (category) => DropdownMenuItem(
+                      value: category,
+                      child: Text(category.name.toUpperCase()),
+                    ),
+                  )
+                  .toList(),
+              onChanged: (value) {
+                setState(() {
+                  if (value == null) {
+                    return;
+                  }
+                  _selectedCategory = value;
+                });
+              },
+            ),
+            const Spacer(),
             TextButton(
               onPressed: () {
                 Navigator.pop(context);
