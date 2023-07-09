d55d614ad6b8fa01d251d45f4e21ea7228861a3f
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-09 14:41:08 +0330

    Create Controller For TextField

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,10 +10,12 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
-  var _enterdTitle = '';
+  final _titleController = TextEditingController();
 
-  void _saveTitleInput(String inputValue) {
-    _enterdTitle = inputValue;
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
   }
 
   @override
@@ -22,7 +24,7 @@ class _NewExpenseState extends State<NewExpense> {
       padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
-          onChanged: _saveTitleInput,
+          controller: _titleController,
           maxLength: 50,
           decoration: const InputDecoration(
             label: Text('Title'),
@@ -32,7 +34,7 @@ class _NewExpenseState extends State<NewExpense> {
           children: [
             ElevatedButton(
               onPressed: () {
-                print(_enterdTitle);
+                print(_titleController.text);
               },
               child: const Text('Save Expense'),
             )
