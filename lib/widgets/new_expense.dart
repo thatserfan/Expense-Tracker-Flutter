8480f9818a7a557adc738ef8a6cd38f860256a5f
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-09 14:31:34 +0330

    Getting Userinput Text

--- lib/widgets/new_expense.dart
+++ lib/widgets/new_expense.dart
@@ -10,16 +10,33 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enterdTitle = '';
+
+  void _saveTitleInput(String inputValue) {
+    _enterdTitle = inputValue;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(children: [
         TextField(
+          onChanged: _saveTitleInput,
           maxLength: 50,
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             label: Text('Title'),
           ),
+        ),
+        Row(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                print(_enterdTitle);
+              },
+              child: const Text('Save Expense'),
+            )
+          ],
         ),
       ]),
     );
