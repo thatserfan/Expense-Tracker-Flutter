288611b9714d5f6e6bab171dc0150b211077a423
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-10 16:58:43 +0330

    Add Undo option when expense removed

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -41,9 +41,26 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+
     setState(() {
       _registeredExpenses.remove(expense);
     });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text("Expense Deleted"),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
