dd65ce23eb8b2ad9fe5425f5faf1e7764a3da344
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-11 21:06:12 +0330

    Add forcategory for expensebucket

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -35,7 +35,15 @@ class Expense {
 }
 
 class ExpenseBucket {
-  const ExpenseBucket({required this.category, required this.expenses});
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
 
   final Category category;
   final List<Expense> expenses;
