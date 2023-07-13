e5806cb6d67341132a3fc0a7cb9a57bdf0f5c3f3
Erfan Farahbakhsh <itserfan.farahbakhsh@gmail.com>
2023-07-13 22:14:58 +0330

    Remove orientations - Make unique UI in landscape orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(2555, 96, 59, 181),
@@ -12,17 +12,37 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) {
-    runApp(
-      MaterialApp(
-        darkTheme: ThemeData.dark().copyWith(
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer),
+        ),
+      ),
+      theme: ThemeData().copyWith(
           useMaterial3: true,
-          colorScheme: kDarkColorScheme,
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
           cardTheme: const CardTheme().copyWith(
-            color: kDarkColorScheme.secondaryContainer,
+            color: kColorScheme.secondaryContainer,
             margin: const EdgeInsets.symmetric(
               horizontal: 16,
               vertical: 8,
@@ -30,38 +50,18 @@ void main() {
           ),
           elevatedButtonTheme: ElevatedButtonThemeData(
             style: ElevatedButton.styleFrom(
-                backgroundColor: kDarkColorScheme.primaryContainer,
-                foregroundColor: kDarkColorScheme.onPrimaryContainer),
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
           ),
-        ),
-        theme: ThemeData().copyWith(
-            useMaterial3: true,
-            colorScheme: kColorScheme,
-            appBarTheme: const AppBarTheme().copyWith(
-              backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer,
-            ),
-            cardTheme: const CardTheme().copyWith(
-              color: kColorScheme.secondaryContainer,
-              margin: const EdgeInsets.symmetric(
-                horizontal: 16,
-                vertical: 8,
-              ),
-            ),
-            elevatedButtonTheme: ElevatedButtonThemeData(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: kColorScheme.primaryContainer,
-              ),
-            ),
-            textTheme: ThemeData().textTheme.copyWith(
-                  titleLarge: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: kColorScheme.onSecondaryContainer,
-                    fontSize: 16,
-                  ),
-                )),
-        home: const Expenses(),
-      ),
-    );
-  });
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 16,
+                ),
+              )),
+      home: const Expenses(),
+    ),
+  );
+  // });
 }
